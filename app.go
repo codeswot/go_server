a060d12f0dbff1756d54ef7178cec1c418f14ea9
Mubarak Ibrahim <40618838+elMubarak@users.noreply.github.com>
2021-11-15 14:47:49 +0100

    initial GO

---
+++ app.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	genfunctions "github.com/elmubarak/go_crud/genFunctions"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", genfunctions.FetchAll)
+	router.GET("/:id", genfunctions.FetchById)
+	router.POST("/", genfunctions.CreateStudent)
+	router.PUT("/:id", genfunctions.UpdateStudent)
+	router.DELETE("/:id", genfunctions.DeleteStudent)
+	fmt.Println("Server listening to port 3000")
+	http.ListenAndServe(":3000", router)
+}
